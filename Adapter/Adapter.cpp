bda47f524bfa35fc321190a714bf910ecfd686d6
AnikinAV <ankinaleksey2003@mail.ru>
2024-04-19 17:04:09 +0400

    Создан адаптор класса , адаптирующий класс "Связный список" к итерфейсу класса "Стек"

--- Adapter/Adapter.cpp
+++ Adapter/Adapter.cpp
@@ -1,20 +1,117 @@
-﻿// Adapter.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
+﻿
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+//Целевой класс "Стек"
+template <typename T>
+class Stack {
+public:
+    Stack() {};
+    virtual void push(T element) = 0;
+    virtual T pop() = 0;
+    virtual bool isEmpty() const = 0;
+    virtual ~Stack() {}
+};
+
+//Класс узла списка
+template <typename T>
+class Node  {
+public:
+    T data;
+    Node <T>* next;
+    Node(T value) : data(value), next(nullptr) {}
+};
+
+// Адаптируемый класс "Связный список"
+template <typename T>
+class myList {
+private:
+    Node<T>* head;
+public:
+    myList() : head(nullptr) {}
+    void addToEnd(T element) {
+        Node<T>* newNode = new Node<T>(element);
+        if (head == nullptr) {
+            head = newNode;
+        }
+        else {
+            Node<T >* current = head;
+            while (current->next != nullptr) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+    T removeFromEnd() {
+        if (head != nullptr) {
+            if (head->next == nullptr) {
+                T tmp = head->data;
+                delete head;
+                head = nullptr;
+                return tmp;
+            }
+            else {
+                Node<T>* current = head;
+                while (current->next->next != nullptr) {
+                    current = current->next;
+                }
+                T tmp = current->next->data;
+                delete current->next;
+                current->next = nullptr;
+                return tmp;
+            }
+        }
+    }
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+    ~myList() {
+        Node<T>* current = head;
+        while (current != nullptr) {
+            Node<T>* temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+};
+
+// Адаптер класса
+template <typename T> 
+class StackAdapter : public Stack<T> ,private myList<T>{
+public:
+    void push(T element) override 
+    {
+        myList<T>::addToEnd(element);
+    }
+    T pop() override 
+    {
+        return myList<T>::removeFromEnd();
+    }
+    bool isEmpty() const override 
+    {
+        return myList<T>::isEmpty();
+    }
+};
+int main() {
+   /* Stack<int>* stackint = new StackAdapter<int>();
+    stackint->push(1);
+    stackint->push(2);
+    stackint->push(3);
+    stackint->push(4);
+    while (!stackint->isEmpty()) {
+        cout << stackint->pop() << endl;
+    }*/
+    Stack<string>* stackstring = new StackAdapter<string>();
+    stackstring->push("A");
+    stackstring->push("B");
+    stackstring->push("C");
+    stackstring->push("D");
+    while (!stackstring->isEmpty()) {
+        cout << stackstring->pop() << endl;
+    }
+
+    
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
